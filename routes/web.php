7856f2dab3f6baa6addef82d03a35bee82e6cc3c
3a932111 <3a932111@gm.student.ncut.edu.tw>
2022-11-04 09:58:54 +0800

    練習 1-1&1-2 新增 home.html&設定route回傳頁面

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*練習 1-2 設定回傳home*/
+Route::get('home', function () {
+    return view('home');
+});
